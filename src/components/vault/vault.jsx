ab90abdadb6bcebdc7e26bc5ddef729bf46a6ff5
Enomer <aprehatny@yahoo.com>
2018-05-31 20:05:06 -0700

    Styling and grid adjustments to vault column

--- src/components/vault/vault.jsx
+++ src/components/vault/vault.jsx
@@ -7,15 +7,17 @@ class MyVault extends Component {
   render() {
     return (
       <main className="grid-x" style={{height: '95vh'}}>
-        <div className="cell large-4">
-          <div className="grid-y grid-frame align-top">
+
+        <div className="cell large-4 vaultColumn">
+          <div className="grid-y grid-frame">
+
             <div className="cell large-6">
               <div className="grid-y grid-frame align-center">
                 <div className="cell large-5">
                   <div className="grid-x align-middle align-center" style={{height: '100%'}}>
                     <div className="grid-x cell large-10" style={{height: '100%'}}>
-                      <h1 className="cell">My Vault</h1>
-                      <input className="cell" type="text"  />
+                      <h1 className="cell myVault">My Vault</h1>
+                      <input className="cell vaultSearch" type="text"  />
                       <a style={{alignSelf: 'center'}} className="button primary shrink cell">
                         Show Filters
                       </a>
@@ -24,17 +26,26 @@ class MyVault extends Component {
                 </div>
               </div>
             </div>
-            <div className="cell large-6 align-center">
-              <div style={{width: '100%'}} className="grid-x cell large-6 align-center">
-                <div className="grid-x cell large-10 quickShare" >
-                  <h2>Quick Share</h2>
-                  <input type="text" className="" />
-                  <input type="text" className="" />
-                  <a className="button">Share</a>
+
+            <div className="cell large-6">
+              <div className="grid-y grid-frame align-center">
+                <div className="cell large-7">
+                  <div className="grid-x align-middle align-center" style={{height: '100%'}}>
+                    <div className="grid-x cell large-10 align-center quickShare" style={{height: '100%'}}>
+                      <h3 className="cell quickShareHeader">Quick Share</h3>
+                      <input className="cell" type="text"  />
+                      <input className="cell" type="text"  />
+                      <a style={{alignSelf: 'center'}} className="button primary shrink cell quickShareBtn">
+                        Share
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+
           </div>
+
         </div>
 
 
