7e44b18802f6f226b7da587d31558e1678a83ff1
AidanPrehatny <aprehatny@yahoo.com>
2018-06-10 10:14:33 -0700

    cleaning up project folder, deleting repetitions

--- server/middlewares/routes/projects.js
+++ server/middlewares/routes/projects.js
@@ -33,6 +33,8 @@ const Project = require('../../models/project');
 //     });
 // });
 
+// *** WHEN YOU CLICK FOLDER REVEAL THESE PROJECTS ***//
+
 router.get('/folder_projects', (req, res) => {
   Project.find({
     user: req.query.ownerId,
@@ -93,77 +95,6 @@ router.post('/', (req, res, next) => {
     });
 });
 
-// FIND A PROJECT THEN MAP IT TO A FOLDER - GIVE A PRODUCT A FOLDER, THEN FIND ALL PRODUCTS WITH THAT FOLDER?
-// router.post('/project', (req, res, next) => {
-//   Project.findById(req.body.projectId)
-//     .then((projects) => {
-//       if (!projects) {
-//         return res.status(404).json({
-//           message: 'Project not found'
-//         });
-//       }
-//       const folder = new Folder({
-//         _id: mongoose.Types.ObjectId(),
-//         category: req.body.category,
-//         ownerId: req.body.ownerId,
-//         projects: req.body.projectId
-//       });
-//       return folder.save();
-//     })
-//     .then((result) => {
-//       console.log(result);
-//       res.status(201).json({
-//         message: 'Project stored',
-//         createdFolder: {
-//           _id: result._id,
-//           projects: result.projects,
-//           ownerId: result.ownerId,
-//           category: result.category
-//         },
-//         request: {
-//           type: 'GET',
-//           url: `http://localhost:3000/api/folders/${result._id}`
-//         }
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: err
-//       });
-//     });
-// });
-
-// *** WHEN YOU CLICK FOLDER REVEAL THESE PROJECTS ***//
-// TODO: .find() method should look for matching owner id
-router.get('/', (req, res, next) => {
-  Project.find()
-    // .select('ownerId folderName _id category')
-    .exec()
-    .then((docs) => {
-      const response = {
-        projects: docs.map(() => req.body
-        // ({
-        //   ownerId: doc.ownerId,
-        //   folderName: doc.folderName,
-        //   category: doc.category,
-        //   _id: doc._id,
-        //   request: {
-        //     type: 'GET',
-        //     url: `http://localhost:3000/folders/${doc._id}`
-        //   }
-        // })
-        )
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
 
 // FIND SPECIFIC FOLDER
 router.get('/:projectId', (req, res, next) => {
@@ -190,26 +121,26 @@ router.get('/:projectId', (req, res, next) => {
     });
 });
 
-// DELETE FOLDER
+// DELETE PROJECT
 
-router.delete('/:orderId', (req, res, next) => {
-  Folder.remove({ _id: req.params.orderId })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: 'Folder deleted',
-        request: {
-          type: 'POST',
-          url: 'http://localhost:3000/orders',
-          body: { productId: 'ID', quantity: 'Number' }
-        }
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+// router.delete('/:orderId', (req, res, next) => {
+//   Folder.remove({ _id: req.params.orderId })
+//     .exec()
+//     .then((result) => {
+//       res.status(200).json({
+//         message: 'Folder deleted',
+//         request: {
+//           type: 'POST',
+//           url: 'http://localhost:3000/orders',
+//           body: { productId: 'ID', quantity: 'Number' }
+//         }
+//       });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({
+//         error: err
+//       });
+//     });
+// });
 
 module.exports = router;
