11733856c63da92bc5af7bbf277a01f9cf09f298
Aidan P <aprehatny@yahoo.com>
2018-06-13 21:29:27 -0700

    all actions now tsx | removed additional javascript tests

--- app/containers/ProjectContainer/actions.tsx
+++ app/containers/ProjectContainer/actions.tsx
@@ -2,6 +2,6 @@ export const GET_PROJECT_ID = 'GET_PROJECT_ID';
 export const GET_PROJECT = 'GET_PROJECT';
 export const PUT_PROJECT = 'PUT_PROJECT';
 
-export const getProjectId = (projectId) => ({ type: GET_PROJECT_ID, projectId });
+export const getProjectId = (projectId:string) => ({ type: GET_PROJECT_ID, projectId });
 export const getProject = () => ({ type: GET_PROJECT });
-export const putProject = (projectData) => ({ type: PUT_PROJECT, projectData });
+export const putProject = (projectData:{}[]) => ({ type: PUT_PROJECT, projectData });
