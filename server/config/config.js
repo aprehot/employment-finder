90b4d525aebc75f8342577f4734167bf21e3edcb
Aidan Prehatny <34518260+AidanPrehatny@users.noreply.github.com>
2018-10-02 20:02:03 -0700

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,6 @@ const config = {
   production: {
     SECRET: process.env.SECRET,
     DATABASE: process.env.MONGODB_URI
-  },
-  default: {
-    SECRET: 'SUPERSECRETPASSWORD123',
-    // DATABASE: 'mongodb://localhost:27017/projects'
-    DATABASE: 'mongodb+srv://AniAdmin:discoverani@ani-prototype-wzu37.mongodb.net/projects'
   }
 };
 
